acc625d1fbd90e6f5e1272175dbee3b6ae17f6b9
Jonathan Alba Videira <jonathan.videira@gmail.com>
2021-12-25 20:12:54 -0300

    Refreshing token

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,35 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
+import { parseCookies, setCookie } from 'nookies';
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error: AxiosError) => {
+    if (error.response?.status === 401) {
+      if (error.response.data?.code === 'token.expired') {
+        const cookies = parseCookies();
+
+        const {
+          data: { token, refreshToken },
+        } = await api.post('/refresh', {
+          refreshToken: cookies['nextAuth.refreshToken'],
+        });
+
+        setCookie(null, 'nextAuth.token', token, {
+          maxAge: 60 * 60 * 24 * 30,
+          path: '/',
+        });
+
+        setCookie(null, 'nextAuth.refreshToken', refreshToken, {
+          maxAge: 60 * 60 * 24 * 30,
+          path: '/',
+        });
+
+        api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+      }
+    }
+  }
+);
